88990dec49013c9f5703191c21d08ea683ede838
ychoudhary <yogesh.choudhary@capita.co.uk>
2016-09-18 20:30:14 +0530

    Added a HTML5 page

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path')
 app.listen(8275, function(){
    console.log("Server started on localhost:8275"); 
 });
 app.get('/', function(req, res){
-   res.send("Hello Node Express"); 
+    console.log(__dirname);
+    console.log(__filename);
+    res.sendFile(path.join(__dirname + '/index.html')); 
 });
