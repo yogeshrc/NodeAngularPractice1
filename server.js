7f715a18dd4c3c3aec8a9bc3d3769c212c4dfd6e
ychoudhary <yogesh.choudhary@capita.co.uk>
2016-10-06 22:11:46 +0530

    Finally a working index.html

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+app.use(express.static(__dirname));
+
 var path = require('path')
 app.listen(8275, function(){
    console.log("Server started on localhost:8275"); 
@@ -7,5 +10,6 @@ app.listen(8275, function(){
 app.get('/', function(req, res){
     console.log(__dirname);
     console.log(__filename);
-    res.sendFile(path.join(__dirname + '/index.html')); 
+    // res.sendFile(path.join(__dirname + '/index.html')); 
+    res.render(path.join(__dirname + '/index.html'));
 });
