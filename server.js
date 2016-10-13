ceabd6a5ee50aae1d56bbc64b12d42bdbad13d73
ychoudhary <yogesh.choudhary@capita.co.uk>
2016-10-13 06:14:56 +0530

    Separated projects in a different module

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var app = express();
+
+require('./app/projects')(app);
+// require('./app/projects');
+
 var path = require('path');
 app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/app/views'));
 
+console.log("__dirname = " + __dirname);
+
 app.listen(8275, function(){
    console.log("Server started on localhost:8275"); 
 });
+
 app.get('/', function(req, res){
     res.render(path.join(__dirname + '/app/views/index.html'));
-});
-app.get('/projects', function(req, res){
-   res.json({
-       'title': 'My Hobby Projects',
-       'projects': [ 'NodeJS', 'AngularJS', 'ExpressJS', 'Arduino', 'GitHub']
-   });
 });
